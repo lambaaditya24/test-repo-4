ed056521f675cf20afbb5b0b361d9945a51066d4
adla1996 <aditya@w4m.ai>
2024-07-16 00:29:03 -0700

    Initial Commit

---
+++ src-old/config.ts
@@ -0,0 +1,4 @@
+export const WebsiteName: string = "W4M.ai - Lead Generation"
+export const WebsiteBaseUrl: string = "https://saas-test.w4m.ai"
+export const WebsiteDescription: string =
+  "Get ahead of your competition."
---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const WebsiteName: string = "W4M.ai - AI Automation"
+export const WebsiteBaseUrl: string = "https://W4M.ai"
+export const WebsiteDescription: string =
+  "Save Time, Improve your Life and Regain your Humanity. Simply the Best AI Automation."
